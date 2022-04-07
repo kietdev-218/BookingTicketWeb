b648158268962c6d8dac7047b8f1649e0af4260f
Nguyen Anh kiet <nguyenanhkiet210800@gmail.com>
2022-04-07 21:27:51 +0700

    1

--- BookingTicketWeb/src/main/java/com/web/service/impl/CategoryServiceImpl.java
+++ BookingTicketWeb/src/main/java/com/web/service/impl/CategoryServiceImpl.java
@@ -2,27 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.web.service.impl;
+package com.web.service.Impl;
 
 import com.web.pojo.Category;
+import com.web.repository.CategoryRepository;
 import com.web.service.CategoryService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.web.repository.CategoryRepository;
 
 /**
  *
- * @author kietdev
+ * @author Man
  */
 @Service
 public class CategoryServiceImpl implements CategoryService{
     @Autowired
-    private CategoryRepository categoryRepository; 
-    
+    private CategoryRepository categoryRepository;
     @Override
     public List<Category> getCategories() {
         return this.categoryRepository.getCategories();
     }
+    
     
 }

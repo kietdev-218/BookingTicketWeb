605d1926d09797cc8aa8504883e459461fed0939
Mẫn Hồ <72002135+HoTrinhMan@users.noreply.github.com>
2022-04-09 09:33:27 +0700

    dm

--- BookingTicketWeb/src/main/java/com/web/service/Impl/CategoryServiceImpl.java
+++ BookingTicketWeb/src/main/java/com/web/service/Impl/CategoryServiceImpl.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.web.service.impl;
+package com.web.service.Impl;
 
 import com.web.pojo.Category;
 import com.web.repository.CategoryRepository;
@@ -13,16 +13,16 @@ import org.springframework.stereotype.Service;
 
 /**
  *
- * @author kietdev
+ * @author Man
  */
 @Service
 public class CategoryServiceImpl implements CategoryService{
     @Autowired
-    private CategoryRepository categoryRepository; 
-    
+    private CategoryRepository categoryRepository;
     @Override
     public List<Category> getCategories() {
         return this.categoryRepository.getCategories();
     }
+    
     
 }
